1d68a65e1bc5cbe5bc9fdf1948178b15910357d0
Aaron <dyhong2002@gmail.com>
2026-05-03 00:14:11 +0800

    Revert "branch test"
    This reverts commit c33e6f1c8ad7da9bb7874cab89694ec868bf01b8.

--- adaptive_clock_dd.m
+++ adaptive_clock_dd.m
@@ -24,8 +24,9 @@ pulse_half_len = 50;
 carrier_freq = 20;
 
 %% === TRANSMITTER ===
-str = 'hello 123$%^&ABZ';
-disp("message:")
+str = 'p';
+str = 'hello 123$%^&ABZ'
+disp("message")
 disp(str)
 [symbols, num_symbols, tx_passband, t] = transmit(str, oversamp, pulse_half_len, carrier_freq, symbol_period);
 
